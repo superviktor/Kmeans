0eb429c0feb5b91fbc9465e32b063a9e152fb68e
Viktor Prykhidko <victor.pryhidko@gmail.com>
2017-10-16 19:04:53 +0300

    Parallel Kmenas increased speed in 8 times

--- KMeansClustering/KMeansClustering/Algs/ManualParallelKmeans.cs
+++ KMeansClustering/KMeansClustering/Algs/ManualParallelKmeans.cs
@@ -4,22 +4,126 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace KMeansClustering.Algs
 {
-	public delegate void NewMethodWrapper(int from, int to);
-	public delegate void NewMethodWrapper11(List<DataItem> items,int from, int to);
+	public delegate void ExecuteParallel(int from, int to);
 	public class ManualParallelKmeans
 	{
 		public List<DataItem> defaultData = new List<DataItem>();
 		public List<DataItem> normalizedDataToCluster = new List<DataItem>();
 		public List<DataItem> clusters = new List<DataItem>();
 		public int numberOfClusters = 0;
-		NewMethodWrapper methodWrapper;
-		NewMethodWrapper11 methodWrapper11;
+		ExecuteParallel executeParallelWrapper;
+		bool changed;
+		bool success;
+
 		public void SetDataDefaultData(List<DataItem> data)
 		{
 			defaultData = data;
+		}
+
+		//Gaussian Normalization 
+		//public void NormalizeData(List<DataItem> defaultDataParam)
+		//{
+
+		//	double xSum = 0;
+		//	double ySum = 0;
+
+		//	Parallel.ForEach<DataItem>(defaultDataParam, (dataItem) =>
+		//	{
+		//		xSum += dataItem.X;
+		//		ySum += dataItem.Y;
+		//	});
+
+		//	double xMean = xSum / defaultDataParam.Count;
+		//	double yMean = ySum / defaultDataParam.Count;
+		//	double xAndxMeanSubPow = 0;
+		//	double yAndyMeanSubPow = 0;
+
+		//	Parallel.ForEach<DataItem>(defaultDataParam, (dataItem) =>
+		//	{
+		//		xAndxMeanSubPow += Math.Pow(dataItem.X - xMean, 2);
+		//		yAndyMeanSubPow += Math.Pow(dataItem.Y - yMean, 2);
+		//	});
+
+		//	double xSD = xAndxMeanSubPow / defaultDataParam.Count;
+		//	double ySD = yAndyMeanSubPow / defaultDataParam.Count;
+
+		//	Parallel.ForEach<DataItem>(defaultDataParam, (dataItem) =>
+		//	{
+		//		normalizedDataToCluster.Add(new DataItem()
+		//		{
+		//			X = (dataItem.X - xMean) / xSD,
+		//			Y = (dataItem.Y - yMean) / ySD
+		//		});
+		//	});
+
+		//}
+
+		private void NormalizeData(List<DataItem> defaultDataParam)
+		{
+			double xSum = 0;
+			double ySum = 0;
+			foreach (var dataItem in defaultDataParam)
+			{
+				xSum += dataItem.X;
+				ySum += dataItem.Y;
+			}
+			double xMean = xSum / defaultDataParam.Count;
+			double yMean = ySum / defaultDataParam.Count;
+			double xAndxMeanSubPow = 0;
+			double yAndyMeanSubPow = 0;
+			foreach (var dataItem in defaultDataParam)
+			{
+				xAndxMeanSubPow += Math.Pow(dataItem.X - xMean, 2);
+				yAndyMeanSubPow += Math.Pow(dataItem.Y - yMean, 2);
+			}
+			double xSD = xAndxMeanSubPow / defaultDataParam.Count;
+			double ySD = yAndyMeanSubPow / defaultDataParam.Count;
+			foreach (var dataItem in defaultDataParam)
+			{
+				normalizedDataToCluster.Add(new DataItem()
+				{
+					X = (dataItem.X - xMean) / xSD,
+					Y = (dataItem.Y - yMean) / ySD
+				});
+			}
+		}
+
+		private void InitializeCentroids()
+		{
+			Random rnd = new Random(numberOfClusters);
+			Parallel.For(0, normalizedDataToCluster.Count, (i) =>
+			{
+				normalizedDataToCluster[i].Cluster = defaultData[i].Cluster = rnd.Next(0, numberOfClusters);
+			});
+
+		}
+
+		private bool UpdateMeans()
+		{
+			if (EmptyCluster(normalizedDataToCluster))
+				return false;
+			var groupsToComputeMeans = normalizedDataToCluster.GroupBy(s => s.Cluster).OrderBy(s => s.Key);
+			int clusterIndex = 0;
+			double x = 0;
+			double y = 0;
+			foreach (var group in groupsToComputeMeans)
+			{
+				foreach (var item in group)
+				{
+					x += item.X;
+					y += item.Y;
+				}
+				clusters[clusterIndex].X = x / group.Count();
+				clusters[clusterIndex].Y = y / group.Count();
+				clusterIndex++;
+				x = 0;
+				y = 0;
+			}
+			return true;
 		}
 
 		private bool EmptyCluster(List<DataItem> data)
@@ -37,216 +141,7 @@ namespace KMeansClustering.Algs
 			return false;
 		}
 
-		public int MinIndex(double[] distances)
-		{
-			if (distances.Length <= 0)
-			{
-				return -1;
-			}
-			int minIndex = 0;
-			double minDistance = distances[0];
-			for (int i = 0; i < distances.Length; ++i)
-			{
-				if (distances[i] < minDistance)
-				{
-					minDistance = distances[i];
-					minIndex = i;
-				}
-			}
-			return minIndex;
-		}
-
-		double xSum;
-		double ySum;
-		double xMean;
-		double yMean;
-		double xAndxMeanSubPow;
-		double yAndyMeanSubPow;
-		double xSD;
-		double ySD;
-
-		private void GetXSum( int from, int to)
-		{
-			for (int i = from; i < to; i++)
-			{
-				xSum += defaultData[i].X;
-			}
-
-		}
-
-		private void GetYSum( int from, int to)
-		{
-			for (int i = from; i < to; i++)
-			{
-				ySum += defaultData[i].Y;
-			}
-		}
-
-		private void GetXAndxMeanSubPow( int from, int to)
-		{
-			for (int i = from; i < to; i++)
-			{
-				xAndxMeanSubPow += Math.Pow(defaultData[i].X - xMean, 2);
-			}
-		}
-
-		private void GetYAndYMeanSubPow(int from, int to)
-		{
-			for (int i = from; i < to; i++)
-			{
-				yAndyMeanSubPow += Math.Pow(defaultData[i].Y - yMean, 2);
-			}
-
-		}
-
-		private void SetNormalizedDataArray(int from, int to)
-		{
-			for (int i = from; i < to; i++)
-			{
-				normalizedDataToCluster.Add(new DataItem()
-				{
-					X = (defaultData[i].X - xMean) / xSD,
-					Y = (defaultData[i].Y - yMean) / ySD
-				});
-			}	
-		}
-
-		private void SetRandomDataItemCluster(int from, int to)
-		{
-			Random rnd = new Random(numberOfClusters);
-			for (int i = from; i < to; i++)
-			{
-				normalizedDataToCluster[i].Cluster = defaultData[i].Cluster = rnd.Next(0, numberOfClusters);
-			}
-		}
-
-		private void GetXYSum(List<DataItem> items,int from, int to)
-		{
-			for (int i = from; i < to; i++)
-			{
-				x += items[i].X;
-				y += items[i].Y;
-			}
-			
-		}
-
-		private void ExecuteParallel(int threads, NewMethodWrapper method)
-		{
-
-			int part = defaultData.Count / threads;
-			Thread[] ts = new Thread[threads];
-			for (int k = 0; k < ts.Length; k++)
-			{
-				int from = k * part;
-				int to = k * part + part;
-				ts[k] = new Thread(() => method.Invoke(from, to));
-			}
-			for (int k = 0; k < ts.Length; k++)
-			{
-				ts[k].Start();
-			}
-			for (int k = 0; k < ts.Length; k++)
-			{
-				ts[k].Join();
-			}
-		}
-
-		private void ExecuteParallel2(List<DataItem> list,int threads, NewMethodWrapper11 method)
-		{
-
-			int part = list.Count / threads;
-			Thread[] ts = new Thread[threads];
-			for (int k = 0; k < ts.Length; k++)
-			{
-				int from = k * part;
-				int to = k * part + part;
-				ts[k] = new Thread(() => method.Invoke(list,from, to));
-			}
-			for (int k = 0; k < ts.Length; k++)
-			{
-				ts[k].Start();
-			}
-			for (int k = 0; k < ts.Length; k++)
-			{
-				ts[k].Join();
-			}
-		}
-
-		//Gaussian Normalization 
-		private void NormalizeData(List<DataItem> defaultDataParam)
-		{
-			methodWrapper = new NewMethodWrapper(GetXSum);
-			ExecuteParallel(4, methodWrapper);
-			methodWrapper = new NewMethodWrapper(GetYSum);
-			ExecuteParallel(4, methodWrapper);
-			xMean = xSum / defaultData.Count;
-			yMean = ySum / defaultData.Count;
-			methodWrapper = new NewMethodWrapper(GetXAndxMeanSubPow);
-			ExecuteParallel(4, methodWrapper);
-			methodWrapper = new NewMethodWrapper(GetYAndYMeanSubPow);
-			ExecuteParallel(4, methodWrapper);
-			Thread.Sleep(100);
-			xSD = xAndxMeanSubPow / defaultData.Count;
-			ySD = yAndyMeanSubPow / defaultData.Count;
-			methodWrapper = new NewMethodWrapper(SetNormalizedDataArray);
-			ExecuteParallel(4, methodWrapper);
-		}
-
-		private void InitializeCentroids()
-		{		
-			methodWrapper = new NewMethodWrapper(SetRandomDataItemCluster);
-			ExecuteParallel(4, methodWrapper);		
-		}
-
-		double x;
-		double y;
-		public bool UpdateMeans()
-		{
-			if (EmptyCluster(normalizedDataToCluster))
-				return false;
-			var groupsToComputeMeans = normalizedDataToCluster.GroupBy(s => s.Cluster).OrderBy(s => s.Key);
-			int clusterIndex = 0;
-			x = 0;
-			y = 0;
-			foreach (var group in groupsToComputeMeans)
-			{
-				var groupList = group.ToList();
-				methodWrapper11 = new NewMethodWrapper11(GetXYSum);
-				ExecuteParallel2(groupList, 4, methodWrapper11);
-
-				clusters[clusterIndex].X = x / group.Count();
-				clusters[clusterIndex].Y = y / group.Count();
-				clusterIndex++;
-				x = 0;
-				y = 0;
-			}
-			return true;
-
-			//if (EmptyCluster(normalizedDataToCluster))
-			//	return false;
-			//var groupsToComputeMeans = normalizedDataToCluster.GroupBy(s => s.Cluster).OrderBy(s => s.Key);
-			//int clusterIndex = 0;
-			//double x = 0;
-			//double y = 0;
-			//foreach (var group in groupsToComputeMeans)
-			//{
-			//	foreach (var item in group)
-			//	{
-			//		x += item.X;
-			//		y += item.Y;
-			//	}
-			//	clusters[clusterIndex].X = x / group.Count();
-			//	clusters[clusterIndex].Y = y / group.Count();
-			//	clusterIndex++;
-			//	x = 0;
-			//	y = 0;
-			//}
-			//return true;
-
-		}
-		
-
-		public bool UpdateClusterMembership()
+		private bool UpdateClusterMembership()
 		{
 			EuclideanDistance ed = new EuclideanDistance();
 			bool changed = false;
@@ -271,32 +166,72 @@ namespace KMeansClustering.Algs
 			return true;
 		}
 
-
-
-		public void Cluster(List<DataItem> data, int numberOfClusters)
+		private int MinIndex(double[] distances)
 		{
-			bool changed = true;
-			bool success = true;
+			if (distances.Length <= 0)
+			{
+				return -1;
+			}
+			int minIndex = 0;
+			double minDistance = distances[0];
+			for (int i = 0; i < distances.Length; ++i)
+			{
+				if (distances[i] < minDistance)
+				{
+					minDistance = distances[i];
+					minIndex = i;
+				}
+			}
+			return minIndex;
+		}
+
+		private void ExecuteParallel(int threads, ExecuteParallel wrapper, int max)
+		{
+			int part = defaultData.Count / threads;
+			Thread[] ts = new Thread[threads];
+			for (int k = 0; k < ts.Length; k++)
+			{
+				int from = k * part;
+				int to = k * part + part;
+				ts[k] = new Thread(() => wrapper.Invoke(from, to));
+			}
+			for (int k = 0; k < ts.Length; k++)
+			{
+				ts[k].Start();
+			}
+			for (int k = 0; k < ts.Length; k++)
+			{
+				ts[k].Join();
+			}
+		}
+
+		private void Update(int f, int t)
+		{
+			success = UpdateMeans();
+			changed = UpdateClusterMembership();
+		}
+
+		private void Cluster(List<DataItem> data, int numberOfClusters)
+		{
+			changed = true;
+			success = true;
 			InitializeCentroids();
 			int maxIteration = data.Count * 10;
-			int threshold = 0;
-			while (success && changed && threshold < maxIteration)
+			if (success && changed)
 			{
-				++threshold;
-				success = UpdateMeans();
-				changed = UpdateClusterMembership();
+				executeParallelWrapper = new ExecuteParallel(Update);
+				ExecuteParallel(4, executeParallelWrapper, maxIteration);
 			}
 		}
 
 		public void Execute()
 		{
 			NormalizeData(defaultData);
-			Thread.Sleep(100);
+
 			for (int i = 0; i < numberOfClusters; i++)
 			{
 				clusters.Add(new DataItem() { Cluster = i });
 			}
-
 			Cluster(normalizedDataToCluster, numberOfClusters);
 		}
 	}
